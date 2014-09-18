ec5e4e61facf745b251e883d35cde602d8efa4d7
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-19 00:00:43 +0200

    slug added to replay

--- django_simple_forum/urls.py
+++ django_simple_forum/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('',
     url(r'^$', 'django_simple_forum.views.index', name='forum-index'),
     url(r'^(?P<slug>[-\w]+)/$', 'django_simple_forum.views.forum', name='forum-detail'),
     url(r'^(?P<slug>[-\w]+)/(?P<topic_id>\d+)/$', 'django_simple_forum.views.topic', name='topic-detail'),
-    url(r'^(?P<topic_id>\d+)/reply/$', 'django_simple_forum.views.post_reply', name='reply'),
+    url(r'^(?P<slug>[-\w]+)/(?P<topic_id>\d+)/reply/$', 'django_simple_forum.views.post_reply', name='reply'),
     url(r'(?P<slug>[-\w]+)/newtopic/$', 'django_simple_forum.views.new_topic', name='new-topic'),
 )
