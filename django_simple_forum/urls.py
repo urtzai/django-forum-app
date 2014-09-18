af4214096bdda4fec1d760cdc50540d5e3794c5a
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-18 23:12:22 +0200

    url fix

--- django_simple_forum/urls.py
+++ django_simple_forum/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'django_simple_forum.views.index', name='forum-index'),
-    url(r'^(\d+)/$', 'django_simple_forum.views.forum', name='forum-detail'),
-    url(r'^topic/(\d+)/$', 'django_simple_forum.views.topic', name='topic-detail'),
+    url(r'^(?P<forum>[-\w]+)/$', 'django_simple_forum.views.forum', name='forum-detail'),
+    url(r'^(?P<forum>[-\w]+)/(?P<topic>[-\w]+)/$', 'django_simple_forum.views.topic', name='topic-detail'),
     url(r'^reply/(\d+)/$', 'django_simple_forum.views.post_reply', name='reply'),
     url(r'newtopic/(\d+)/$', 'django_simple_forum.views.new_topic', name='new-topic'),
 )
