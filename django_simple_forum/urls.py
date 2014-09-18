fbfd9ced620ba3e2c93b0bcbf944ea8628efcc3a
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-09-18 23:40:20 +0200

    urls changes and fixed

--- django_simple_forum/urls.py
+++ django_simple_forum/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('',
     url(r'^$', 'django_simple_forum.views.index', name='forum-index'),
     url(r'^(?P<slug>[-\w]+)/$', 'django_simple_forum.views.forum', name='forum-detail'),
     url(r'^(?P<slug>[-\w]+)/(\d+)/$', 'django_simple_forum.views.topic', name='topic-detail'),
-    url(r'^reply/(\d+)/$', 'django_simple_forum.views.post_reply', name='reply'),
-    url(r'newtopic/(\d+)/$', 'django_simple_forum.views.new_topic', name='new-topic'),
+    url(r'^(\d+)/reply/$', 'django_simple_forum.views.post_reply', name='reply'),
+    url(r'(?P<slug>[-\w]+)/newtopic/$', 'django_simple_forum.views.new_topic', name='new-topic'),
 )
