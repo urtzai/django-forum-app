1bf2a20b3a6eda0c2230ae367b756a26d5ebaaba
Urtzi Odriozola <uodriozola@codesyntax.com>
2014-10-16 12:29:28 +0200

    Update views.py

--- django_simple_forum/views.py
+++ django_simple_forum/views.py
@@ -68,7 +68,7 @@ def post_reply(request, slug, topic_id):
     if topic.last_post():
         form_title = 'Re: ' + topic.last_post().title.replace('Re: ','')
     
-    default_data = {'title': form_title,'body':''}
+    default_data = {'title': form_title,'body':'Zure erantzuna...'}
     form = PostForm(default_data)
     
     if request.method == 'POST':
