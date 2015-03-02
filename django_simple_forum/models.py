9148748518219eacf79c4347d37368b30583128f
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-03-02 17:18:53 +0100

    Update models.py

--- django_simple_forum/models.py
+++ django_simple_forum/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.conf import settings
-from gamerauntsia.gamer.models import GamerUser as User
+try:
+    from django.contrib.auth import get_user_model
+except ImportError: # django < 1.5
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
 from photologue.models import Photo
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
