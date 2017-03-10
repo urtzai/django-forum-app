1f100f2af490d914619bd8044c441e6f0c2e429c
csmant <csmant@anboto>
2017-03-10 13:03:27 +0100

    test mechanism

---
+++ runtests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+APP_NAME = 'django_simple_forum'
+
+settings.configure(
+    DEBUG=True,
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+        }
+    },
+    USE_TZ=True,
+    ROOT_URLCONF='{0}.tests'.format(APP_NAME),
+    SITE_ID=1,
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.admin',
+        'django.contrib.sites',
+        APP_NAME,
+    ),
+    TEMPLATE_CONTEXT_PROCESSORS=(
+        "django.contrib.auth.context_processors.auth",
+        "django.core.context_processors.debug",
+        "django.core.context_processors.i18n",
+        "django.core.context_processors.media",
+        "django.core.context_processors.request",
+    ),
+)
+
+if hasattr(django, 'setup'):
+    django.setup()
+TestRunner = get_runner(settings)
+test_runner = TestRunner()
+failures = test_runner.run_tests([APP_NAME])
+if failures:
+    sys.exit(failures)
