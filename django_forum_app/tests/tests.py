1331d659cd4aeb37e78437c661e4fa4a0530d063
csmant <csmant@anboto>
2017-03-14 17:48:00 +0100

    Fixing tests

--- django_forum_app/tests/tests.py
+++ django_forum_app/tests/tests.py
@@ -17,7 +17,7 @@ class BasicTest(TestCase):
         user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
         cat = Category.objects.create(order=1, title="General")
         forum = Forum.objects.create(title="The Beatles", slug="the-beatles", category=cat, creator=user)
-        topic = Topic.objects.create(title="Which is the first album's name?", forums=forum, creator=user)
+        topic = Topic.objects.create(title="Which is the first album's name?", forums=[forum], creator=user)
         Post.objects.create(title="My opinion", creator=user, topic=topic, body="I think is Please Please Me")
 
     def test_forum_index(self):
